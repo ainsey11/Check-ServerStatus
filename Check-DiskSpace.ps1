79014e5a647885845cd667a7b96a42c551d95dae
Ainsey11 <robert@ainsey11.com>
2016-09-01 09:52:31 +0100

    Added my comments in

--- Check-DiskSpace.ps1
+++ Check-DiskSpace.ps1
@@ -1,13 +1,17 @@
-﻿Import-Module ActiveDirectory
+﻿#Imports required modules
+Import-Module ActiveDirectory
+
+#Sets some locations and gets some handy info from AD
 $serverlistlocation = "C:\Scripting\Logs\serverlist.txt"
 $Serverlist = Get-ADComputer -Filter {(Operatingsystem -Like "Windows Server*") -And (Enabled -eq "True")} | foreach { $_.Name } | Out-File $serverlistlocation
 
+#The function itself
 function DriveSpace-StandardServer {
 
 param( [string] $strComputer) 
 
-$dbservername = "10.159.100.104"
-$databasename = "ThingsGoingOnTest1"
+$dbservername = "<Enter DB Server Name Here"
+$databasename = "<Enter DB Name Here>"
 
 
 # Does the server responds to a ping (otherwise the WMI queries will fail)
@@ -27,6 +31,7 @@ if ($result.protocoladdress) {
 		$Drive = $disk.DeviceID
        "$strComputer - $Drive - $PercentFree"
 
+       # Creates flags if following criteria is met
        if ($PercentFree -le 10){
        [int]$Warning_State = 1
        [int]$Error_State = 0}
@@ -39,22 +44,28 @@ if ($result.protocoladdress) {
        [int]$Warning_State = 0
        [int]$Error_State = 0
        }
+       #gets date for timestamp in sql so we can truncate past 30 days
        $timestamp = Get-Date
-
+    #MAGICAAAL SQL VOOODDOOOOO YO
     $SqlConnection = New-Object System.Data.SqlClient.SQLConnection
+    #Connection String
     $SqlConnection.ConnectionString = "Server=$dbservername;Database=$databasename;Integrated Security=SSPI;"
     $ConnectionString = "Server=$dbservername;Database=$databasename;Integrated Security=SSPI;"
+    #Insert Statement to be pushed into the database
     $insertstatement = "INSERT INTO Disk_space(SpaceFree,DriveLetter,Warning_State,Error_State,Servername,timestamp)
                         VALUES ('$($PercentFree)','$($Drive)','$($Warning_State)','$($Error_State)','$($strComputer)','$($timestamp)')"
+    # Opens Connection, Pushes the data then closes the connection to keep it tidy
     $SqlConnection.Open()
     $SqlCmd = New-Object "System.Data.SqlClient.SqlCommand" ($insertstatement,$SqlConnection)
     $SqlCmd.ExecuteNonQuery()
     $SqlConnection.Close()
+    # Have a little nap sql, you worked hard today.
     Start-Sleep 2
 
     }
-    foreach ($computer in cat $serverlistlocation) {DriveSpace-StandardServer "$computer"}
+    
 }
 }
 
-foreach ($computer in cat $serverlistlocation) {DriveSpace-StandardServer "$computer"}
+foreach ($computer in cat $serverlistlocation) {DriveSpace-StandardServer "$computer"
+}
