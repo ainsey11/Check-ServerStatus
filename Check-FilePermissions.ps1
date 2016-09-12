bb603804e8a501607ec0fe531d6be96a57a815b8
Ainsey11 <robert@ainsey11.com>
2016-09-12 16:55:13 +0100

    duplicate, epic fail on my part

--- Check-FilePermissions.ps1
+++ Check-FilePermissions.ps1
@@ -1,23 +1,16 @@
-﻿$dbservername = "PC01390"
-$databasename = "FilePermissions"
-$timestamp = Get-Date
-$RootPath = "D:\Documents"
+﻿
+#Setting up variables, kinda obvious tbh
+$dbservername = "PC01390" #yeah, you got it kiddo?
+$databasename = "FilePermissions" #Kinda obvious what this one does
+$timestamp = Get-Date #For the SQL timestamp column, handy when doing select statements
+$RootPath = "D:\Documents" #The folder you want to scan for permissions. Note that it only does the top 4 folders, if you want more, add another wildcard on line 20
 
-#MAGICAAAL SQL VOOODDOOOOO YO
-$SqlConnection = New-Object System.Data.SqlClient.SQLConnection
-#Connection String
-$SqlConnection.ConnectionString = "Server=$dbservername;Database=$databasename;Integrated Security=SSPI;"
-$ConnectionString = "Server=$dbservername;Database=$databasename;Integrated Security=SSPI;"
-#Insert Statement to be pushed into the database
-$insertstatement = "INSERT INTO permissions(Folderpath,Identityreference,AccessControlType,Isinherited,Inheritanceflags,PropagationFlags,timestamp)
-                    VALUES ('$($folder.FullName)','$($ACL.IdentityReference)','$($ACL.AccessControlType)','$($ACL.IsInherited)','$($ACL.InheritanceFlags)','$($ACL.PropagationFlags)','$($timestamp)')"
-
-
-$Folders = dir $RootPath\*\*\*\ | where {$_.psiscontainer -eq $true}
+$Folders = dir $RootPath\*\*\*\ | where {$_.psiscontainer -eq $true} #Creates list of folders to get ACL's for
 
 foreach ($Folder in $Folders){
     $ACLs = get-acl $Folder.fullname | ForEach-Object { $_.Access  }
     Foreach ($ACL in $ACLs){
+
     #MAGICAAAL SQL VOOODDOOOOO YO
     $SqlConnection = New-Object System.Data.SqlClient.SQLConnection
     #Connection String
