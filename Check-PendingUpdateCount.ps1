25265ca67a17b00d2d4ab35a47a2ffe217ac4179
Ainsey11 <robert@ainsey11.com>
2016-09-27 09:11:38 +0100

    added sql logging information

--- Check-PendingUpdateCount.ps1
+++ Check-PendingUpdateCount.ps1
@@ -1,5 +1,8 @@
 ﻿#Imports required modules
 Import-Module ActiveDirectory
+$dbservername = "PC00934"
+$databasename = "ThingsGoingOnTest1"
+$timestamp = Get-Date
 
 #Sets some locations and gets some handy info from AD
 $today = Get-Date
@@ -11,4 +14,18 @@ $updatesession =  [activator]::CreateInstance([type]::GetTypeFromProgID("Microso
 $updatesearcher = $updatesession.CreateUpdateSearcher()  
 $searchresult = $updatesearcher.Search("IsInstalled=0")  
 Write-Output "$((Get-Date).ToShortTimeString()): PATCHING MESSAGE - There are $($searchresult.Updates.count) updates via WSUS to be processed on $($server)"  
+    #MAGICAAAL SQL VOOODDOOOOO YO
+    $SqlConnection = New-Object System.Data.SqlClient.SQLConnection
+    #Connection String
+    $SqlConnection.ConnectionString = "Server=$dbservername;Database=$databasename;Integrated Security=SSPI;"
+    $ConnectionString = "Server=$dbservername;Database=$databasename;Integrated Security=SSPI;"
+    #Insert Statement to be pushed into the database
+    $insertstatement = "INSERT INTO windows_updates(servername,updatescount,timestamp)
+                        VALUES ('$($server)','$($searchresult.Updates.count)','$($timestamp)')"
+    # Opens Connection, Pushes the data then closes the connection to keep it tidy
+    $SqlConnection.Open()
+    $SqlCmd = New-Object "System.Data.SqlClient.SqlCommand" ($insertstatement,$SqlConnection)
+    $SqlCmd.ExecuteNonQuery()
+    $SqlConnection.Close()
+
 }
